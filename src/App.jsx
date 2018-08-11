44a14017b660394286dcdb4dcc8d81bfe00b1139
dongkai <1142071620@qq.com>
2018-08-11 17:03:06 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import {Button} from 'antd-mobile'
+
+export default function App() {
+    return <Button type='primary'>Hello react</Button>
+}
